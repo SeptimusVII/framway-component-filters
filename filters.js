16a2489b58cf4f94fe6bb6d93adcca500538a9b5
SeptimusVII <vansteene.quentin@gmail.com>
2025-10-30 18:58:35 +0100

    move autocomplete related features behind a markup condition

--- filters.js
+++ filters.js
@@ -3,16 +3,16 @@ module.exports = function(app){
     //Filters.debug = true;
     Filters.createdAt      = "2.0.0";
     Filters.lastUpdate     = "2.7.0";
-    Filters.version        = "1.1.0";
+    Filters.version        = "1.1.1";
     // Filters.factoryExclude = true;
     // Filters.loadingMsg     = "This message will display in the console when component will be loaded.";
     // Filters.requires       = [];
 
     Filters.prototype.onCreate = function(){
         var filters = this;
-        filters.$filters = filters.$el.find('.filter');
-        filters.$container = $(filters.getData('container'));
-        filters.$items = filters.$container.find(filters.getData('items'));
+        filters.$filters   = filters.$el.find('.filter').length                       ? filters.$el.find('.filter')                       : false;
+        filters.$container = $(filters.getData('container')).length                   ? $(filters.getData('container'))                   : false;
+        filters.$items     = filters.$container.find(filters.getData('items')).length ? filters.$container.find(filters.getData('items')) : false;
         filters.blnAutocomplete = (filters.blnAutocomplete !== undefined) ? filters.blnAutocomplete : filters.getData('autocomplete',false);
         filters.submit          = (filters.submit !== undefined)          ? filters.submit          : filters.getData('submit',false);
         filters.blnReset        = (filters.blnReset !== undefined)        ? filters.blnReset        : filters.getData('reset',false);
@@ -36,6 +36,27 @@ module.exports = function(app){
                     arrFilters = [ ...new Set(arrFilters.sort())];
                     $.each(arrFilters,function(){
                         $(filter).append('<option value="'+this+'">'+this+'</option>')
+       
+
+        if (filters.$filters && filters.$container && filters.$items) {
+            if (filters.blnAutocomplete) {
+                filters.$filters.each(function(i,filter){
+                    if (filter.nodeName == 'SELECT') {
+                        if ($(filter).attr('data-label') != '')
+                            $(filter).prepend('<option value="">'+$(filter).attr('data-label')+'</option>');
+                        var arrFilters = [];
+                        filters.$items.each(function(){
+                            if($(this).attr('data-'+$(filter).attr('data-filter')))
+                                arrFilters.push($(this).attr('data-'+$(filter).attr('data-filter')));
+                        });
+                        arrFilters = [ ...new Set(arrFilters.sort())];
+                        $.each(arrFilters,function(){
+                            $(filter).append('<option value="'+this+'">'+this+'</option>')
+                        });
+                        $(filter).trigger('change');
+                    }
+                });
+            }
                     });
                     $(filter).trigger('change');
                 }
@@ -61,10 +82,40 @@ module.exports = function(app){
                             filters.$items.not('[data-'+target+'="'+value+'"]').addClass('hidden');
                         if (filter.nodeName == 'INPUT' && filter.getAttribute('type') == 'text')
                             filters.$items.not('[data-'+target+'*="'+value.toLowerCase()+'"]').addClass('hidden');
+
+            if (filters.blnReset) {
+                filters.$el.append('<span class="filters__reset"><i class="fa fa-times"></i></span>');
+                $('.filters__reset').on('click',function(){
+                    filters.$filters.val('').trigger('change');
+                });
+            }
+
+            if (filters.submit && filters.$el.closest('form').length) {
+                filters.$filters.filter('select').on('change',function(e){
+                    if (filters.$el.closest('form').length){
+                        filters.$el.closest('form').trigger('submit');
+                        return false
                     }
                 });
-            });
+            } else {
+                filters.$filters.on('change keyup',function(e){
+                    // console.log('filter change');
+                    filters.$items.removeClass('hidden');
+                    filters.$filters.each(function(i,filter){
+                        var $filter = $(filter);
+                        var target = $filter.attr('data-filter');
+                        var value = $filter.val();
+                        if (value != ""){
+                            if (filter.nodeName == 'SELECT')
+                                filters.$items.not('[data-'+target+'="'+value+'"]').addClass('hidden');
+                            if (filter.nodeName == 'INPUT' && filter.getAttribute('type') == 'text')
+                                filters.$items.not('[data-'+target+'*="'+value.toLowerCase()+'"]').addClass('hidden');
+                        }
+                    });
+                });
+            }
         }
+        
 
     }
     return Filters;
